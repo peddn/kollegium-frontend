c6660862192f4259ea8b4e517eb6f0abcbcfeab7
peddn <public@peddn.net>
2021-11-02 14:50:04 +0100

    cleanup

--- src/js/components/navigation/PricipalNavbarItem.vue
+++ src/js/components/navigation/PricipalNavbarItem.vue
@@ -2,7 +2,9 @@
   <div class="navbar-item has-dropdown is-hoverable">
     <a class="navbar-link">Schulleitung</a>
     <div class="navbar-dropdown">
-      <div class="navbar-item has-text-weight-bold">SCHREIBEN ZUR KENNTNISNAHME</div>
+      <div class="navbar-item has-text-weight-bold">
+        SCHREIBEN ZUR KENNTNISNAHME
+      </div>
       <router-link to="/letters" class="navbar-item">
         <span class="icon-text">
           <span class="icon">
@@ -11,7 +13,7 @@
           <span>Schreiben verwalten</span>
         </span>
       </router-link>
-            <router-link to="/letters/create" class="navbar-item">
+      <router-link to="/letters/create" class="navbar-item">
         <span class="icon-text">
           <span class="icon">
             <i class="fas fa-file-alt"></i>
--- src/js/components/navigation/TeacherNavbarItem.vue
+++ src/js/components/navigation/TeacherNavbarItem.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="navbar-item has-dropdown is-hoverable">
-      <a class="navbar-link">Dienstlich</a>
-      <div class="navbar-dropdown">
-        <router-link to="/perusals" class="navbar-item"
-          >Kenntnisnahme</router-link
-        >
+  <div class="navbar-item has-dropdown is-hoverable">
+    <a class="navbar-link">Dienstlich</a>
+    <div class="navbar-dropdown">
+      <div class="navbar-item has-text-weight-bold">
+        ZUR KENNTNISNAHME
       </div>
+      <router-link to="/perusals" class="navbar-item">
+        <span class="icon-text">
+          <span class="icon">
+            <i class="fas fa-eye"></i>
+          </span>
+          <span>Schreiben</span>
+        </span>
+      </router-link>
     </div>
+  </div>
 </template>
 
 <script>
